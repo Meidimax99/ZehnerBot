0a593c53d9419630a3a0e0aa34e4c7824ebfd2e4
roku <leonhard.kohn@stud.uni-bamberg.de>
2023-05-18 15:46:40 +0200

    wip(user)

--- modules/model/user.py
+++ modules/model/user.py
@@ -8,14 +8,42 @@ class verified_status (Enum):
     excuse = 3
 
 
-class user:
-    name: str
-    verified: verified_status
-    proof: str
-    day_list = []
+class weekdays (Enum):
+    Monday = 0
+    Tuesday = 1
+    Wednesday = 2
+    Thursday = 3
+    Friday = 4
+    Saturday = 5
+    Sunday = 6
 
-    def create(self, name: str, proof: str, day_list=[]):
-        self.name = name
-        self.verified = verified_status.uncertain
+
+class user:
+    def __init__(self, name: str, verified: bool, proof: str, day_list: weekdays = []):
+        self.name: str = name
+        self.verified: bool = verified
+        self.proof: str = proof
+        self.day_list = [day_list]
+
+    def update_verified_status(self, verified: bool, proof: str):
+        self.verified = verified
         self.proof = proof
-        day_list = day_list
+
+    def change_day_list(self, day_list: weekdays = []):
+        self. day_list = day_list
+
+    def add_day_to_list(self, weekday: weekdays):
+        self.day_list.append(weekday)
+
+    def print_day_list(self):
+        print(self.day_list)
+
+    def print_user_info():
+        print()
+
+
+leo = user("leo", True, "123", [weekdays.Monday, weekdays.Friday])
+leo.update_verified_status(False, "321")
+leo.change_day_list([weekdays.Sunday])
+leo.add_day_to_list(weekdays.Thursday)
+leo.print_day_list()
