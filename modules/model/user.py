91de8d9f0c4df093fdfef184c8a75414e3081d8d
roku <leonhard.kohn@stud.uni-bamberg.de>
2023-05-18 14:59:58 +0200

    add user

--- modules/model/user.py
+++ modules/model/user.py
@@ -1,5 +1,21 @@
+from enum import Enum
+
+
+class verified_status (Enum):
+    uncertain = 0
+    on_time = 1
+    too_late = 2
+    excuse = 3
+
+
 class user:
-    name,
-    verified,
-    proof,
-    day_list
+    name: str
+    verified: verified_status
+    proof: str
+    day_list = []
+
+    def create(self, name: str, proof: str, day_list=[]):
+        self.name = name
+        self.verified = verified_status.uncertain
+        self.proof = proof
+        day_list = day_list
