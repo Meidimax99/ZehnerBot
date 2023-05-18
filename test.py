e5f1aa3afcae9beadf0551780be4d35a191feff0
Paulmue0 <muellerpaul2000@live.de>
2023-05-18 12:13:15 +0200

    simple python bot test

---
+++ test.py
@@ -0,0 +1,20 @@
+from dotenv import load_dotenv
+
+import discord
+
+load_dotenv()
+
+DISCORD_API_KEY = os.getenv('DISCORD_API_KEY')
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
+
+    async def on_message(self, message):
+        print(f'Message from {message.author}: {message.content}')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+client.run(DISCORD_API_KEY)
